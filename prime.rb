99d0bd574b34f9412f019042389f2da28cb099ff
Pbsmith82 <pbsmith82@gmail.com>
2020-09-13 11:54:09 -0400

    prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
+require'pry'
 # Add  code here!
+def prime?(number)
+    if number < 2 then return false
+    #binding.pry
+    end
+    (2..(number - 1)).each do |dddd|
+        if (number % dddd) == 0
+         return false
+        end
+       end
+        true
+     end
