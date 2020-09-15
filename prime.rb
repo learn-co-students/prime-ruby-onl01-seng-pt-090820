9f039aa5e0117482b47543f58adfdf7bd7e1cad0
DavidRMorphew <morphewd@umich.edu>
2020-09-15 16:51:20 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,12 +1,18 @@
 require 'pry'
 
+# def prime?(num)
+#     number_array = []
+#     i=2
+#     while i < num
+#         number_array << i
+#         i += 1         
+#     end
+#     number_array.none? { |dividend| num % dividend == 0} && num != 1 && num > 0
+# end
+
 def prime?(num)
-    number_array = []
-    i=2
-    while i < num
-        number_array << i
-        i += 1         
-    end
+    dividers = (2..(num-1)).to_a unless num <= 1
     # binding.pry
-    number_array.none? { |dividend| num % dividend == 0} && num != 1 && num > 0
+    num > 1 && dividers.none? { |non_prime_dividend| num % non_prime_dividend == 0}
 end
+
