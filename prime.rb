90070928ff57acbb73304839da74c4097dce52eb
Kristian Joos <kjoos22@gmail.com>
2020-09-16 21:19:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+require 'pry'
+
+def prime?(number)
+  range = (2..number).to_a
+  
+  if number <= 1
+    return false
+  end
+  range.each do |num|
+    if number % num == 0 && num != number
+      return false
+    end
+  end
+  true
+end
