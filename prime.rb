f3f1c90becada9ddb017396a1d03c4dc3b27aa22
Pbsmith82 <pbsmith82@gmail.com>
2020-09-17 13:08:43 -0400

    update language

--- prime.rb
+++ prime.rb
@@ -4,8 +4,8 @@ def prime?(number)
     if number < 2 then return false
     #binding.pry
     end
-    (2..(number - 1)).each do |dddd|
-        if (number % dddd) == 0
+    (2..(number - 1)).each do |range|
+        if (number % range) == 0
          return false
         end
        end
