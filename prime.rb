a2d359c5bde0ded13fbce5bc7820e1b43281aaf5
Nattida Wong <nattidac.wong@gmail.com>
2020-09-18 21:50:32 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+
+def prime?(num)
+  if num < 2
+    false
+  else
+
+    # if num is divisible by a number other than 1 of itself
+    (2..num - 1).to_a.all? do |n|
+      num % n != 0 #evaluating if num divisible (prime)
+    end
+  end
+end
