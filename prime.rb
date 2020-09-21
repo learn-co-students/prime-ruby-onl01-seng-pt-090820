40d1c329b05f918094ab58b1c491a0ee30429ce4
tmurrell2020 <littlerell12@gmail.com>
2020-09-20 23:10:06 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(numbers)
+    if numbers < 2
+        false
+    else
+        (2..numbers - 1).to_a.all? do |n|
+            numbers % n != 0
+        end
+    end
+end
